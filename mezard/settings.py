3465e26e02ccdf5d8b8c6218182ba1db0b31600d
mezeiser <mezardini@gmail.com>
2023-03-22 20:32:07 +0100

    sixth commit

--- mezard/settings.py
+++ mezard/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mezard.onrender.com', 'mezardini.xyz']
+ALLOWED_HOSTS = ['mezard.onrender.com', 'mezardini.xyz', '127.0.0.1']
 
 
 # Application definition
@@ -121,16 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-ADMIN_MEDIA_PREFIX = '/static/admin/'   
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR, 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
