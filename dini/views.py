cea5dc9b5e2359ed8b75ae1fd639c8c906295895
mezeiser <mezardini@gmail.com>
2023-09-26 14:02:56 +0100

    worked on views and templates

--- dini/views.py
+++ dini/views.py
@@ -4,6 +4,14 @@ from django.core.mail import send_mail
 # Create your views here.
 
 def home(request):
+    visitor_ip = request.META.get('REMOTE_ADDR')
+    send_mail(
+            'New Visitor',
+            'A visitor ' + visitor_ip + ' has been on your portfolio website',
+            'settings.EMAIL_HOST_USER',
+            ['mezardini@gmail.com'],
+            fail_silently=False,
+        )
     if request.method == 'POST':
         subject = request.POST['subject']
         email = request.POST['email']
